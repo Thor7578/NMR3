6f8e878a3d5d9cf105460c6563968f9e2ecdcc55
miklmaar <mikk01d3@stud.kea.dk>
2020-05-28 21:47:11 +0200

    Added checks for Date-class and exceptions if wrong arguments are given.

--- src/main/java/com/example/demo/models/Date.java
+++ src/main/java/com/example/demo/models/Date.java
@@ -5,7 +5,10 @@ public class Date {
     private int month;
     private int day;
 
-    public Date (int year, int month, int day){
+    public Date (int year, int month, int day) throws IllegalArgumentException {
+        if(!checkIfValid(day,month,year)){
+            throw new IllegalArgumentException();
+        }
         this.year = year;
         this.month = month;
         this.day = day;
@@ -36,9 +39,81 @@ public class Date {
         this.day = day;
     }
 
+    public boolean checkIfValid(int day, int month, int year){
+        int monthdays = 0;
+        if((day < 1) || (day > 31)){
+            return false;
+        }
+
+        if((month < 1) || (month > 12)){
+            return false;
+        }
+
+        switch(month){
+            case 1:
+                monthdays = 31;
+                break;
+            case 2:
+                if(leapYearCheck(year)){
+                    monthdays = 29;
+                } else {
+                    monthdays = 28;
+                }
+                break;
+            case 3:
+                monthdays = 31;
+                break;
+            case 4:
+                monthdays = 30;
+                break;
+            case 5:
+                monthdays = 31;
+                break;
+            case 6:
+                monthdays = 30;
+                break;
+            case 7:
+                monthdays = 31;
+                break;
+            case 8:
+                monthdays = 31;
+                break;
+            case 9:
+                monthdays = 30;
+                break;
+            case 10:
+                monthdays = 31;
+                break;
+            case 11:
+                monthdays = 30;
+                break;
+            case 12:
+                monthdays = 31;
+                break;
+        }
+
+        if(day > monthdays){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean leapYearCheck(int year){
+        if((year%4)==0){
+            if((year%400)==0){
+                return true;
+            } else if((year%100)==0) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
     public int dateDiff(Date endDate){      // Ufærdig metode, der skal udregne differencen mellem to datoer
         return 1;
-
     }
 
 
